8e46886fd3b0b5d08e72ac75f8a15cf0569c1035
ipratikner <pratiknerlikar@gmail.com>
2021-12-06 14:58:02 +0530

    Added: created BTS and added elements 56 and then 30,70

--- src/main/java/com/bridgelabz/MyBinaryTree.java
+++ src/main/java/com/bridgelabz/MyBinaryTree.java
@@ -1,5 +1,31 @@
 package com.bridgelabz;
 
-public class MyBinaryTree {
+public class MyBinaryTree<K extends Comparable<K>> {
+    private MyBinaryNode<K> root;
 
+    public void add(K key){
+        this.root = this.addRecursive(root,key);
+    }
+
+    private MyBinaryNode<K> addRecursive(MyBinaryNode<K> current,K key){
+        if(current == null)
+            return new MyBinaryNode<>(key);
+        int compareResult = key.compareTo(current.key);
+        if(compareResult == 0) return current;
+        if(compareResult < 0) {
+            current.left = addRecursive(current.left, key);
+        }else {
+            current.right = addRecursive(current.right,key);
+        }
+        return current;
+    }
+
+    public int getSize(){
+        return this.getSizeRecursive(root);
+    }
+
+    public int getSizeRecursive(MyBinaryNode<K> current){
+        return current == null ? 0 : 1 + this.getSizeRecursive(current.left)
+                + this.getSizeRecursive(current.right);
+    }
 }
---
+++ src/test/java/com/bridgelabz/MyBinaryTreeTest.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyBinaryTreeTest {
+    @Test
+    public void givenNumberWhenAddedBinaryTreeShouldReturnSizeThree(){
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        int size =myBinaryTree.getSize();
+        Assertions.assertEquals(3,size);
+    }
+
+}
